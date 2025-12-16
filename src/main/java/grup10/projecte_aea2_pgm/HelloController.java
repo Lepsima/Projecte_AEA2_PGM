9688f613d064fc5344f3d5f70f0f57e6bb6dd2fa
Lepsima <hugo.diaz.morcillo@gmail.com>
2025-12-16 20:50:42 +0100

    Initial commit

---
+++ src/main/java/grup10/projecte_aea2_pgm/HelloController.java
@@ -0,0 +1,14 @@
+package grup10.projecte_aea2_pgm;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module grup10.projecte_aea2_pgm {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens grup10.projecte_aea2_pgm to javafx.fxml;
+    exports grup10.projecte_aea2_pgm;
+}
